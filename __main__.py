e02f37fe8d864cb7e1cb91f8c239c634aa8eb460
Dibble <DeflatedPickle@gmail.com>
2018-05-27 08:44:06 +0100

    Added the interpreter and main

---
+++ __main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""""""
+
+import antlr4
+
+from KoiLexer import KoiLexer
+from KoiParser import KoiParser
+from koi_interpreter import KoiInterpreter
+
+if __name__ == "__main__":
+    lexer = KoiLexer(antlr4.FileStream("examples/Koi/variables.koi"))
+    stream = antlr4.CommonTokenStream(lexer)
+    parser = KoiParser(stream)
+    tree = parser.program()
+
+    interpret = KoiInterpreter()
+    walker = antlr4.ParseTreeWalker()
+    walker.walk(interpret, tree)
---
+++ koi_interpreter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""""""
+from KoiListener import KoiListener
+
+
+class KoiInterpreter(KoiListener):
+    def __init__(self):
+        pass
